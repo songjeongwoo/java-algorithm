646259aba73c99a1ff24857ba461b9be790789c4
jeongwoo <nice920525@naver.com>
2023-08-15 19:14:41 +0900

    add Baekjoon #1157

---
+++ src/baekjoon/B1157.java
@@ -0,0 +1,33 @@
+package src.baekjoon;
+
+import java.util.*;
+
+public class B1157 {
+    public static int[] getAlphabetCount(String str) {
+        int[] count = new int[26];
+        for (int i = 0; i < str.length(); i++) {
+            count[str.charAt(i) - 'A']++;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine().toUpperCase();
+        int[] count = getAlphabetCount(str);
+
+        int maxCount = -1;
+        char maxAlphabet = '?';
+
+        for (int i = 0; i < count.length; i++) {
+            if (count[i] > maxCount) {
+                maxCount = count[i];
+                maxAlphabet = (char)('A' + i);
+            } else if (count[i] == maxCount) {
+                maxAlphabet = '?';
+            }
+        }
+
+        System.out.println(maxAlphabet);
+    }
+}
