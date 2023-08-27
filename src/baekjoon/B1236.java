9fe7b68269e44bac7ae662cce0b9665dd6a8c30a
jeongwoo <nice920525@naver.com>
2023-08-27 20:46:41 +0900

    add Baekjoon #1236 another solving

--- src/baekjoon/B1236.java
+++ src/baekjoon/B1236.java
@@ -9,35 +9,34 @@ public class B1236 {
         int N = sc.nextInt();
         int M = sc.nextInt();
         char[][] map = new char[N][M];
+        // O(N * M)
         for (int i = 0; i < N; i++)
             map[i] = sc.next().toCharArray();
 
         // 1. 각 행/열에 대해 경비원이 있는지 확인한다.
-        int existRowCount = 0;
+        boolean[] existRow = new boolean[N];
+        boolean[] existCol = new boolean[M];
+        // O(N * M)
         for (int i = 0; i < N; i++) {
-            boolean exist = false;
             for (int j = 0; j < M; j++)
                 if (map[i][j] == 'X') {
-                    exist = true;
-                    break;
+                    existRow[i] = true;
+                    existCol[j] = true;
                 }
-            if (exist) existRowCount++;
-        }
-
-        int existColCount = 0;
-        for (int i = 0; i < M; i++) {
-            boolean exist = false;
-            for (int j = 0; j < N; j++)
-                if (map[j][i] == 'X') {
-                    exist = true;
-                    break;
-                }
-            if (exist) existColCount++;
         }
 
         // 2. 보호받지 못하는 행/열의 개수를 구한다.
-        int needRowCount = N - existRowCount;
-        int needColCount = M - existColCount;
+        int needRowCount = N;
+        int needColCount = M;
+        // O(N)
+        for (int i = 0; i < N; i++)
+            if (existRow[i]) needRowCount--;
+        // O(M)
+        for (int i = 0; i < M; i++)
+            if (existCol[i]) needColCount--;
+
+
+        // 시간 복잡도 : O(N * M) + O(N * M) + O(N) + O(M) => O(NM)
 
         // 3. 둘 중 큰 값을 출력한다.
         System.out.println(Math.max(needRowCount, needColCount));
