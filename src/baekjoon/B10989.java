d4f1041aa0bc05d5db16a200db33a130c4523585
jeongwoo <nice920525@naver.com>
2023-09-03 21:47:51 +0900

    add Baekjoon #10989(ft. BufferedReader, BufferedWriter)

--- src/baekjoon/B10989.java
+++ src/baekjoon/B10989.java
@@ -1,22 +1,25 @@
 package src.baekjoon;
 
-import java.util.*;
+import java.io.*;
 
 public class B10989 {
     // 최종 시간복잡도: O(N) = 10,000,000
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int N = sc.nextInt();
+        int N = Integer.parseInt(br.readLine());
         int[] cnt = new int[10001];
         // 시간복잡도: O(N)
         for (int i = 0; i < N; i++)
-            cnt[sc.nextInt()]++;
+            cnt[Integer.parseInt(br.readLine())]++;
         // 시간복잡도: O(max(10,000, N))
         for (int i = 1; i <= 10000; i++) {
             while(cnt[i]-- > 0) {
-                System.out.println(i);
+                bw.write(i + "\n");
             }
         }
+        bw.flush();
+        bw.close();
     }
 }
