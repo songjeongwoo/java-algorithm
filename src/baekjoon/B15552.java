02847cb00939bb446567ea10f39f8c5feeb0e36b
jeongwoo <nice920525@naver.com>
2023-09-03 21:51:10 +0900

    add Baekjoon #15552 - time out(ft. Scanner)

---
+++ src/baekjoon/B15552.java
@@ -0,0 +1,15 @@
+package src.baekjoon;
+
+import java.util.*;
+
+public class B15552 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println(a + b);
+        }
+    }
+}
