9c23a9c2c202d2030d3aadcf27a32daa829629dd
jeongwoo <nice920525@naver.com>
2023-09-03 21:33:14 +0900

    add Baekjoon #10989 - time out(ft. Scanner, System.out.println)

--- src/baekjoon/B10989.java
+++ src/baekjoon/B10989.java
@@ -3,15 +3,20 @@ package src.baekjoon;
 import java.util.*;
 
 public class B10989 {
+    // 최종 시간복잡도: O(N) = 10,000,000
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         int N = sc.nextInt();
-        int[] arr = new int[N];
+        int[] cnt = new int[10001];
+        // 시간복잡도: O(N)
         for (int i = 0; i < N; i++)
-            arr[i] = sc.nextInt();
-        arr = Arrays.stream(arr).sorted().toArray();
-        for (int i : arr) {
-            System.out.println(i);
+            cnt[sc.nextInt()]++;
+        // 시간복잡도: O(max(10,000, N))
+        for (int i = 1; i <= 10000; i++) {
+            while(cnt[i]-- > 0) {
+                System.out.println(i);
+            }
         }
     }
 }
