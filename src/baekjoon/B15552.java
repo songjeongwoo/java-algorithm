28a34e98cfb870e4658396b727daeb576bfa239d
jeongwoo <nice920525@naver.com>
2023-09-03 21:57:36 +0900

    update Baekjoon #B15552(ft. BufferedReader, BufferedWriter)

--- src/baekjoon/B15552.java
+++ src/baekjoon/B15552.java
@@ -1,15 +1,20 @@
 package src.baekjoon;
 
-import java.util.*;
+import java.io.*;
 
 public class B15552 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(br.readLine());
         while (T-- > 0) {
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            System.out.println(a + b);
+            String[] nums = br.readLine().split(" ");
+            int a = Integer.parseInt(nums[0]);
+            int b = Integer.parseInt(nums[1]);
+            bw.write(a + b + "\n");
         }
+        bw.flush();
+        bw.close();
     }
 }
